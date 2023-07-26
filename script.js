9bcc64017d8d7d0769ec8eef7adea6a5b5d31ff1
Aubinmars <aubinmars13@gmail.com>
2023-07-26 11:50:10 +0200

    done script and an html icon

--- script.js
+++ script.js
@@ -1,58 +1,56 @@
-function getComputerChoice() {
-    let choiceNum = Math.floor(Math.random() * 3);
-    switch (choiceNum) {
-      case 0:
-        return "rock";
-      case 1:
-        return "paper";
-      case 2:
-        return "scissors";
-      default:
-        return "";
-    }
+const buttons = document.querySelectorAll('.btn');
+const playerScoreText = document.getElementById('player-score');
+const compScoreText = document.getElementById('comp-score');
+const outputText = document.getElementById('output');
+
+let playerScore = 0;
+let compScore = 0;
+
+function computerPlay() {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    playerScore++;
+    return "You win this round!";
+  } else {
+    compScore++;
+    return "Computer wins this round!";
   }
-  
-  function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase(); // Make playerSelection case-insensitive
-    if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-      return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else if (playerSelection === computerSelection) {
-      return "It's a Tie!";
-    } else {
-      return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
+}
+
+function updateScore() {
+  playerScoreText.textContent = `Player Score: ${playerScore}`;
+  compScoreText.textContent = `Computer Score: ${compScore}`;
+
+  if (playerScore >= 5) {
+    outputText.textContent = "Congratulations! You won the game!";
+    disableButtons();
+  } else if (compScore >= 5) {
+    outputText.textContent = "Computer wins the game! Better luck next time.";
+    disableButtons();
   }
-  
-  function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-  
-    for (let i = 0; i < 5; i++) {
-      const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
-      const computerSelection = getComputerChoice();
-      const result = playRound(playerSelection, computerSelection);
-      console.log(result);
-  
-      if (result.includes("Win")) {
-        playerScore++;
-      } else if (result.includes("Lose")) {
-        computerScore++;
-      }
-    }
-  
-    if (playerScore > computerScore) {
-      console.log("Congratulations! You won the game!");
-    } else if (playerScore < computerScore) {
-      console.log("You lost the game. Better luck next time!");
-    } else {
-      console.log("The game ended in a tie.");
-    }
-  }
-  
-  // Call the game function to start the game
-  game();
-  
+}
+
+function disableButtons() {
+  buttons.forEach(button => button.disabled = true);
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    const playerSelection = this.id;
+    const computerSelection = computerPlay();
+    const result = playRound(playerSelection, computerSelection);
+    outputText.textContent = result;
+    updateScore();
+  });
+});
